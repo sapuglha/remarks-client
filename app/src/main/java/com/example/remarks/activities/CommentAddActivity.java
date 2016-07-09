749e5f8acc8ac0e03adfffc0edd6ba46c5388b63
Tiago Silva <tiago@bright42.com>
2016-07-09 16:04:55 -0600

    Sending Comment instance to remote store using Rest
    - Network requests cannot be executed on main thread, this simple approach spawns an AsyncTask to send the Comment to the server.
    
    An implementation of a Remote Store can be found here: https://github.com/sapuglha/remarks-server.
    
    TODO:
    * Persist the Comment before sending to remote store
    * Remove from local storage after successfully sending the Comment
    * Improve exception handling: send to a crash reporting service (Crashlytics), and log only if on debug build

--- app/src/main/java/com/example/remarks/activities/CommentAddActivity.java
+++ app/src/main/java/com/example/remarks/activities/CommentAddActivity.java
@@ -1,5 +1,6 @@
 package com.example.remarks.activities;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
@@ -10,10 +11,16 @@ import android.widget.Toast;
 
 import com.example.remarks.R;
 import com.example.remarks.models.Comment;
+import com.example.remarks.net.Rest;
+
+import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Response;
 
 public class CommentAddActivity extends AppCompatActivity {
     private static final String TAG = " CommentAddActivity";
@@ -31,9 +38,7 @@ public class CommentAddActivity extends AppCompatActivity {
         }
 
         Comment newComment = Comment.create(commentText, System.currentTimeMillis());
-
-        // TODO: send comment
-        Log.d(TAG, "New comment: " + newComment.toString());
+        new PostCommentTask().execute(newComment);
 
         Toast.makeText(this, R.string.comment_add_activity_toast_message_sent, Toast.LENGTH_LONG).show();
     }
@@ -43,5 +48,37 @@ public class CommentAddActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment_add);
         ButterKnife.bind(this);
+    }
+
+    // Network calls cannot be made on main thread, use a separate thread
+    private class PostCommentTask extends AsyncTask<Comment, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(Comment... comments) {
+            // TODO:
+            // before starting a network request we should persist the comment,
+            // then if network request fails it's possible to retry later
+
+            Rest restHttpClient = new Rest();
+            for (Comment comment : comments) {
+                Call<ResponseBody> call = restHttpClient.send(comment);
+                try {
+                    Response<ResponseBody> response = call.execute();
+                    if (null == response) {
+                        Log.e(TAG, "Unknown error sending comment");
+                    } else if (!response.isSuccessful()) {
+                        Log.e(TAG, String.format("Response http status: %s, body: %s", response.code(), response.body().string()));
+                        return false;
+                    } else {
+                        // TODO: remove the comment from the local storage
+                        Log.d(TAG, String.format("Response http status: %s, body: %s", response.code(), response.body().string()));
+                    }
+                } catch (IOException e) {
+                    // TODO: properly handle exception: log if on debug build, and send to a crash reporting service
+                    e.printStackTrace();
+                }
+            }
+
+            return false;
+        }
     }
 }
