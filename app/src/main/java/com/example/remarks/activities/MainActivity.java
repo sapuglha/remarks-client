951e8b9dbeb62dbce6a4a13a9471ac7bdf15e6e4
Tiago Silva <tiago@bright42.com>
2016-07-09 12:45:30 -0600

    Move MainActivity to activities package

--- app/src/main/java/com/example/remarks/activities/MainActivity.java
+++ app/src/main/java/com/example/remarks/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.remarks;
+package com.example.remarks.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.remarks.R;
 
 public class MainActivity extends AppCompatActivity {
 
