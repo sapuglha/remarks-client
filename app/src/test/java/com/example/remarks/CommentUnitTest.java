f10db1d4bbcec50991ae57201e33a33cf5716518
Tiago Silva <tiago@bright42.com>
2016-07-09 15:54:43 -0600

    New unit test to validate Comment to JSON conversion

--- app/src/test/java/com/example/remarks/CommentUnitTest.java
+++ app/src/test/java/com/example/remarks/CommentUnitTest.java
@@ -1,12 +1,18 @@
 package com.example.remarks;
 
 
+import android.util.Log;
+
 import com.example.remarks.models.Comment;
+import com.ryanharter.auto.value.moshi.AutoValueMoshiAdapterFactory;
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
 
 import org.junit.Test;
 
 import java.util.Locale;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
 
@@ -40,5 +46,19 @@ public class CommentUnitTest {
         String expectedOutput = String.format(Locale.ENGLISH, "Comment{comment=%s, timestamp=%d}", message, timestamp);
 
         assertTrue(expectedOutput.equals(comment.toString()));
+    }
+
+    @Test
+    public void comment_MoshiConversion() throws Exception {
+        Moshi moshi = new Moshi.Builder()
+                .add(new AutoValueMoshiAdapterFactory())
+                .build();
+
+        Comment comment = Comment.create("blah", 1468099366980L);
+
+        JsonAdapter<Comment> jsonAdapter = moshi.adapter(Comment.class);
+
+        String json = jsonAdapter.toJson(comment);
+        assertEquals("{\"comment\":\"blah\",\"timestamp\":1468099366980}", json);
     }
 }
